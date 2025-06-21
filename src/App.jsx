af3468df2f921c6db6fb838dacbfaf1a26a2da19
Martin Lowe <martinlowe673@gmail.com>
2025-06-21 15:51:09 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { ScrollTrigger,SplitText } from "gsap/all"
+import { gsap } from "gsap";
+
+gsap.registerPlugin(ScrollTrigger,SplitText);
+
+const App = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
+
