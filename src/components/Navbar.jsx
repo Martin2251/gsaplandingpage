37266fc789996597e162869e3fc4d1d8e139ab69
Martin Lowe <martinlowe673@gmail.com>
2025-06-21 16:06:38 +0100

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 const Navbar = () => {
   return (
+ <nav>
     <div>
-      
+        <a href="#home" className='flex items-center gap-2'>
+            <p>Velvet Pour</p>
+        </a>
     </div>
+ </nav>
   )
 }
 
