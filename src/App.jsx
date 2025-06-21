b456106b08e82d8649b144fed060e47a906277d2
Martin Lowe <martinlowe673@gmail.com>
2025-06-21 16:02:11 +0100

    so far

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ gsap.registerPlugin(ScrollTrigger,SplitText);
 
 const App = () => {
   return (
-    <div className="flex-center">
-      <h1 className="text-3xl text-indigo-300">hello gsap</h1>
+    <div className="flex-center h-[100vh]">
+      <h1 className="text-3xl text-indigo-300">Hello Gsap</h1>
     </div>
   )
 }
