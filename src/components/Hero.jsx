ab158e0780f23e14243ab7b7f50203b451d01d12
Martin Lowe <martinlowe673@gmail.com>
2025-06-24 08:04:33 +0100

    hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,19 @@ const Hero = () => {
   return (
  <section id="hero" className='noisy'>
     <h1 className='title'>Mojito</h1>
-    <img src="/images/hero-left-leaf.png" alt="" />
+    <img src="/images/hero-left-leaf.png" alt="left-leaf" className='left-leaf' />
+        <img src="/images/hero-right-leaf.png" alt="right-leaf" className='right-leaf' />
+        <div className='body'>
+        <div className='content'>
+            <div className='space-y-5 hidden md:block'>
+            <p>cool. crisp and classicj</p>
+            <p className='subtitle'>
+                Sip the spirit <br /> of summer
+            </p>
+            </div>
+        </div>
+
+        </div>
  </section>
   )
 }
